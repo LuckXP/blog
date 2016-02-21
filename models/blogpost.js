02b3b5ab6827a2cc2914d1b5465caa67ddf2996b
LuckXP <js.sindler@gmail.com>
2016-02-20 18:34:32 -0700

    add basic routes structure for api in routes/blogposts.js

---
+++ models/blogpost.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var BlogPost = new Schema({
+	postDate: {
+		type: Date,
+		default: Date.now,
+		required: true,
+	},
+	postBody: {
+		type: String,
+		required: true,
+	},
+	author: {
+		type: String,
+		required: true,
+	},
+});
+
+module.exports = mongoose.model('BlogPost', BlogPostSchema);
---
+++ routes/blogposts.js
@@ -0,0 +1,71 @@
+var express = require('express');
+var router = express.Router();
+
+var Bear = require('../models/blogpost');
+
+router.route('/blogposts')//post a new blog post
+	.post(function(req, res) {
+		var blogPost = new BlogPost();
+
+		blogPost.postDate = req.body.postDate;
+		blogPost.postBody = req.body.postBody
+		blogPost.author = req.body.author;
+
+		blogPost.save(function(err, blogPost){
+			if(err){
+				console.log(err);
+			} else {
+				res.json(blogPost)
+			}
+		})
+	})
+	.get(function(req, res) {
+		BlogPost.find(function(err, blogPosts){
+			if(err){
+				console.log(err);
+			} else {
+				res.json(blogPost);
+			}
+		})
+	});
+
+// router.route('/bears/:bear_id')
+// 	.get(function(req, res) {
+// 		Bear.findById(req.params.bear_id, function(err, bear) {
+// 			if(err){
+// 				console.log(err);
+// 			} else {
+// 				res.json(bear);
+// 			}
+// 		})	
+// 	})
+// 	.put(function(req, res) {
+// 		Bear.findById(req.params.bear_id, function(err, bear) {
+// 			if(err){
+// 				console.log(err);
+// 			} else {
+// 				bear.name = req.body.name ? req.body.name : bear.name;
+// 				bear.age = req.body.age ? req.body.age : bear.age;
+// 				bear.gender = req.body.gender ? req.body.gender : bear.gender;
+				
+// 				bear.save(function(err){
+// 					if(err){
+// 						console.log(err);
+// 					} else {
+// 						res.json({title: "bear updated"});
+// 					}
+// 				})
+// 			}
+// 		})	
+// 	})
+// 	.delete(function(req, res) {
+// 		Bear.remove({_id :req.params.bear_id}, function(err, bear) {
+// 			if(err){
+// 				console.log(err);
+// 			} else {
+// 				res.json({title: "bear was deleted"});
+// 			}
+// 		})	
+// 	})
+
+module.exports = router;
