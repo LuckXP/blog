6cb2b1b77dd2f7e9b8be810d9d85480a1669833b
LuckXP <js.sindler@gmail.com>
2016-02-25 16:46:07 -0700

    add fully RESTfull api

--- routes/blogposts.js
+++ routes/blogposts.js
@@ -30,43 +30,45 @@ router.route('/blogposts')//post a new blog post
 		})
 	});
 
-// router.route('/bears/:bear_id')
-// 	.get(function(req, res) {
-// 		Bear.findById(req.params.bear_id, function(err, bear) {
-// 			if(err){
-// 				console.log(err);
-// 			} else {
-// 				res.json(bear);
-// 			}
-// 		})	
-// 	})
-// 	.put(function(req, res) {
-// 		Bear.findById(req.params.bear_id, function(err, bear) {
-// 			if(err){
-// 				console.log(err);
-// 			} else {
-// 				bear.name = req.body.name ? req.body.name : bear.name;
-// 				bear.age = req.body.age ? req.body.age : bear.age;
-// 				bear.gender = req.body.gender ? req.body.gender : bear.gender;
+router.route('/blogposts/:blogpost_id')
+	.get(function(req, res) {
+		BlogPost.findById(req.params.blogpost_id, function(err, blogPost) {
+			if(err){
+				console.log(err);
+			} else {
+				res.json(blogPost);
+			}
+		})	
+	})
+	.put(function(req, res) {
+		BlogPost.findById(req.params.blogpost_id, function(err, blogPost) {
+			if(err){
+				console.log(err);
+			} else {
 				
-// 				bear.save(function(err){
-// 					if(err){
-// 						console.log(err);
-// 					} else {
-// 						res.json({title: "bear updated"});
-// 					}
-// 				})
-// 			}
-// 		})	
-// 	})
-// 	.delete(function(req, res) {
-// 		Bear.remove({_id :req.params.bear_id}, function(err, bear) {
-// 			if(err){
-// 				console.log(err);
-// 			} else {
-// 				res.json({title: "bear was deleted"});
-// 			}
-// 		})	
-// 	})
+				blogPost.postDate = req.body.postDate ? req.body.postDate : blogPost.postDate;
+				blogPost.postBody = req.body.postBody ? req.body.postBody : blogPost.postBody;
+				blogPost.author = req.body.author ? req.body.author : blogPost.author;
+				
+				blogPost.save(function(err){
+					if(err){
+						console.log(err);
+					} else {
+						res.json({title: "blogpost updated"});
+					}
+				})
+			}
+		})	
+	})
+	.delete(function(req, res) {
+		BlogPost.remove({_id :req.params.blogpost_id}, function(err, blogPost) {
+			if(err){
+				console.log(err);
+			} else {
+				console.log(blogPost);
+				res.json({title: "blogPost was deleted"});
+			}
+		})	
+	})
 
 module.exports = router;
