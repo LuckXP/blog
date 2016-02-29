68dec2011daa2076e2c34d3518fe575dc4dbf74e
LuckXP <js.sindler@gmail.com>
2016-02-28 20:15:36 -0700

    add posting blogpost bootstrap panels to blogpost.ejs as react element

--- public/app.js
+++ public/app.js
@@ -1,12 +1,57 @@
 console.log('my waist\n👌');
 
 var App = React.createClass({
+    getInitialState: function () {
+    	return { blogPosts: []}
+    },
+    propTypes: {
+    	url: React.PropTypes.string.isRequired,
+  	},
+    loadBlogPostsFromServer: function() {
+    	var self = this;
+    	$.ajax({
+    		url: this.props.url,
+    		method: 'GET',
+    	}).done(function(data) {
+    		console.log(data);
+    		self.setState({
+    			blogPosts: data,
+    		});
+    	});
+    },
+    componentDidMount: function() {
+    	console.log("componentDidMount fired");
+    	this.loadBlogPostsFromServer();
+    },
     render: function() {
         return (
-                <h3> Hello World </h3>
-        )
+            <div> 
+            	<BlogPostPanels blogPosts={this.state.blogPosts} />
+            </div>
+        );
     }
+});
 
+var BlogPostPanels = React.createClass({
+	render: function () {
+		var mapBlogPostToPanel = this.props.blogPosts.
+			map( function(blogPost) {
+				return (
+					<div className="panel panel-default">
+  						<div className="panel-heading">{blogPost.postDate}</div>
+  						<div className="panel-body">
+    						{blogPost.postBody}
+  						</div>
+					</div>
+				)
+		});
+		console.log(mapBlogPostToPanel);
+		return (
+			<div>
+				{mapBlogPostToPanel}
+			</div>
+			);
+	},
 });
 
 React.render(<App url="/api/blogposts" />, document.getElementById('blogposts'));
