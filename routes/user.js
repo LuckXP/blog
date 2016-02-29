f3457d57ec13f6c8ad8188f4c00f803d2be4d49b
LuckXP <js.sindler@gmail.com>
2016-02-29 14:08:57 -0700

    add user sign up

---
+++ routes/user.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+var User = require('../models/user');
+
+// app/routes.js
+module.exports = function(app, passport) {
+
+    app.post('/signup', passport.authenticate('local-signup', {
+        successRedirect: '/', // redirect to the secure profile section
+        failureRedirect: '/signup', // redirect back to the signup page if there is an error
+        failureFlash: true // allow flash messages
+    }));
+
+
+    app.get('/signup', function(req, res) {
+        // render the page and pass in any flash data if it exists
+        res.render('signup.ejs', {
+            message: req.flash('signupMessage')
+        });
+    });
+
+
+};
--- server.js
+++ server.js
@@ -2,14 +2,39 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var BlogPost = require('./models/blogpost');
-
+var passport =require('passport');
 var mongoose =require('mongoose');
 mongoose.connect('mongodb://localhost/blogpost');
+
+var flash = require('connect-flash');
+var session = require('express-session');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(session({
+ secret: 'ilovescotchscotchyscotchscotch'
+})); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(session({
+ cookie: {
+   maxAge: 60000
+ }
+}));
+app.use(flash());
+
+require('./config/passport')(passport);
+// routes ======================================================================
+require('./routes/user.js')(app, passport);
+
 app.use(express.static('public'));
+
+app.use(function(req, res, next) {
+	var user = req.user || "no user";
+	console.log(user);
+	next();
+});
 
 app.set('view engine', 'ejs');
 app.get('/', function(req, res) {
